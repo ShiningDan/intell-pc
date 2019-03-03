414eddcfdb30505a5cdc050afba0c4b03ca266d8
ShiningDan <zhuce.zyc@gmail.com>
2019-03-03 14:00:58 +0800

    feat(master): 修改 CDN 加速域名

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  baseUrl:
-    process.env.NODE_ENV === 'production'
-      ? '//assets-1258401619.cos-website.ap-beijing.myqcloud.com'
-      : '',
+  baseUrl: process.env.NODE_ENV === 'production' ? '//assets-1258401619.file.myqcloud.com' : '',
 }
