18a1dc16d4f2836dc9a0c068bf613eeec9e546d0
ShiningDan <zhuce.zyc@gmail.com>
2019-03-03 13:58:12 +0800

    feat(master): 开启静态资源 CDN 加速

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   baseUrl:
-    process.env.NODE_ENV === 'production' ? '//assets-1258401619.cos.ap-beijing.myqcloud.com' : '',
+    process.env.NODE_ENV === 'production'
+      ? '//assets-1258401619.cos-website.ap-beijing.myqcloud.com'
+      : '',
 }
